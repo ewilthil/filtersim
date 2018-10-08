710adb76ff6f80570f8ed9df5f3ce71a40c076af
Erik Wilthil <erik.wilthil@gmail.com>
2018-10-08 15:52:42 -0400

    Changed to facilitate radar estimates

--- detection_probability_analysis/ais_aspect_angle.py
+++ detection_probability_analysis/ais_aspect_angle.py
@@ -34,21 +34,24 @@ def plot_sample_time_cdf(filenames):
         print "processed {}".format(rosbag)
     plot_cdf(sample_times, ax)
 
-def is_detected(est_list, measurements_all, ownship_pose, measurement_model, gate):
+def is_detected(est_list, measurements_all, measurement_timestamps, ownship_pose, measurement_model, gate):
     # Assumes measurements have been synced with est_list
     detection_indicator = []
     aspect_angle = []
-    previous_timestamp = est_list[0].timestamp
-    for k, measurements in enumerate(measurements_all):
+    estimate_timestamps = [est.timestamp for est in est_list]
+    est_alive_indexes = np.digitize(estimate_timestamps, measurement_timestamps, right=True)
+    if np.any(np.diff(est_alive_indexes) != 1):
+        print "Warning: Diff"
+    for k_est, k in enumerate(est_alive_indexes):#, measurements in enumerate(measurements_all):
+        measurements = measurements_all[k]
         current_position = ownship_pose[1:3,k]
         measurement_model.update_ownship(current_position)
-        current_ais_estimate = est_list[k]
+        current_ais_estimate = est_list[k_est]
         current_ais_velocity = np.array([current_ais_estimate.est_posterior[1], current_ais_estimate.est_posterior[3]])
         if np.linalg.norm(current_ais_velocity) < 0.1:
             continue
         gated_measurements = gate.gate_estimate(current_ais_estimate, measurements, measurement_model)
         detection_indicator.append(1) if len(gated_measurements) > 0 else detection_indicator.append(0)
-        previous_timestamp = current_ais_estimate.timestamp
         aspect_angle.append(calculate_aspect_angle(current_ais_estimate, current_position))
     return detection_indicator, aspect_angle
 
@@ -57,8 +60,26 @@ def detect_based_on_ais(chosen_rosbag, measurement_model, gate, chosen_mmsi):
     measurements_all, measurement_timestamps = add_zero_sets(measurements_all, measurement_timestamps)
     ais_data = autobag.synchronize_track_file_to_timestamps(ais_data, measurement_timestamps, q=0)
     ownship_pose, ownship_twist = autobag.bag2navigation_data(chosen_rosbag, measurement_timestamps)
-    detection_indicator, aspect_angle = is_detected(ais_data[chosen_mmsi], measurements_all, ownship_pose, measurement_model, gate)
-    return detection_indicator, aspect_angle
+    detection_indicator_all, aspect_angle_all = [], []
+    for mmsi in chosen_mmsi:
+        if mmsi not in ais_data.keys():
+            continue
+        est_list = ais_data[mmsi]
+        detection_indicator, aspect_angle = is_detected(est_list, measurements_all, measurement_timestamps, ownship_pose, measurement_model, gate)
+        detection_indicator_all += detection_indicator
+        aspect_angle_all += aspect_angle
+    return detection_indicator_all, aspect_angle_all
+
+def detect_based_on_radar(chosen_rosbag, measurement_model, gate):
+    radar_data, ais_data, measurements_all, measurement_timestamps = autobag.bag2tracking_data(chosen_rosbag, True)
+    measurements_all, measurement_timestamps = add_zero_sets(measurements_all, measurement_timestamps)
+    ownship_pose, ownship_twist = autobag.bag2navigation_data(chosen_rosbag, measurement_timestamps)
+    detection_indicator_all, aspect_angle_all = [], []
+    for track_id, est_list in radar_data.items():
+        detection_indicator, aspect_angle = is_detected(est_list, measurements_all, measurement_timestamps, ownship_pose, measurement_model, gate)
+        detection_indicator_all += detection_indicator
+        aspect_angle_all += aspect_angle
+    return detection_indicator_all, aspect_angle_all
 
 def calculate_aspect_angle(estimate, ownship_position):
     est_pos = np.array([estimate.est_posterior[0], estimate.est_posterior[2]])
@@ -70,9 +91,6 @@ def calculate_aspect_angle(estimate, ownship_position):
     angle = np.arccos(cos_angle)
     if cos_perp_angle < 0:
         angle = -angle
-        print "target view from port side"
-    else:
-        print "target view from starboard side"
     if np.isnan(angle):
         print "ERROR: est_pos={}, est_vel={}, own_pos={}".format(est_pos, est_vel, ownship_position)
     return angle
@@ -91,22 +109,32 @@ def add_zero_sets(measurements_all, measurement_timestamps):
         measurement_timestamps.insert(index, new_timestamp)
     return measurements_all, measurement_timestamps
 
-def cluster_detections(detections, angles, N_bins=18):
-    angles_half = np.linspace(-np.pi, np.pi, N_bins)
-    ang_index = np.digitize(angles, angles_half)
-    detections_half = np.zeros_like(angles_half)
-    tries = np.zeros_like(angles_half)
+def cluster_detections(detections, angles, N_bins, symmetrize=False):
+    new_angles = np.linspace(-np.pi, np.pi, N_bins)
+    ang_index = np.digitize(angles, new_angles)
+    new_detections = np.zeros_like(new_angles)
+    tries = np.zeros_like(new_angles)
     for detection, idx in zip(detections, ang_index):
         tries[idx-1] += 1
-        detections_half[idx-1] += detection
-    P_D = np.zeros_like(angles_half)
-    for k, ang in enumerate(angles_half):
+        new_detections[idx-1] += detection
+    P_D = np.zeros_like(new_angles)
+    for k, ang in enumerate(new_angles):
         if tries[k] > 0:
-            P_D[k] = detections_half[k]/tries[k]
+            P_D[k] = new_detections[k]/tries[k]
         else:
             P_D[k] = 0
-    P_D[-1] = P_D[-2]
-    return angles_half, P_D, tries
+    if symmetrize:
+        for lower_idx in range(int(np.floor(len(new_angles)/2.))):
+            upper_idx = -(lower_idx+1)
+            total_tries = tries[lower_idx]+tries[upper_idx]
+            new_PD = (P_D[lower_idx]*tries[lower_idx]+P_D[upper_idx]*tries[upper_idx])/(1.*total_tries)
+            P_D[upper_idx] = new_PD
+            P_D[lower_idx] = new_PD
+            tries[upper_idx] = total_tries
+            tries[lower_idx] = total_tries
+    else:
+        P_D[-1] = P_D[0]
+    return new_angles, P_D, tries
 
 if __name__ == '__main__':
     chosen_rosbag = '/Users/ewilthil/Documents/autosea_testdata/25-09-2018/filtered_bags/filtered_scenario_11_2018-09-25-14-06-12.bag'
@@ -123,19 +151,22 @@ if __name__ == '__main__':
     detections_all = []
     aspect_angle_all = []
     for chosen_rosbag in all_files:
-        detections, angle = detect_based_on_ais(chosen_rosbag, measurement_model, track_gate, autoais.known_mmsi['KSX_OSD1'])
+        detections, angle = detect_based_on_ais(chosen_rosbag, measurement_model, track_gate, [autoais.known_mmsi['KSX_OSD1']])
+        #detections, angle = detect_based_on_radar(chosen_rosbag, measurement_model, track_gate)
         print "processed {}".format(chosen_rosbag)
         detections_all += detections
         aspect_angle_all += angle
     N_bins=36
-    angles, P_D, num_measurements = cluster_detections(detections_all, aspect_angle_all, N_bins=N_bins)
-    ax = plt.subplot(projection='polar')
-    ax.set_theta_zero_location("N")
-    ax.plot(angles, P_D, 'r')
-    #ax.plot(angles+np.pi, P_D[::-1], 'r')
-    plt.figure()
-    ax = plt.subplot(projection='polar')
-    ax.set_theta_zero_location("N")
-    ax.bar(angles, num_measurements, width=np.pi/(N_bins-1))
-    #ax.bar(angles+np.pi-np.pi/(N_bins-1), num_measurements[::-1], width=np.pi/(N_bins-1))
+    for symmetrize in [True, False]:
+        plt.figure()
+        angles, P_D, num_measurements = cluster_detections(detections_all, aspect_angle_all, N_bins=N_bins, symmetrize=symmetrize)
+        ax = plt.subplot(projection='polar')
+        ax.set_theta_zero_location("N")
+        ax.set_theta_direction(-1)
+        ax.plot(angles, P_D, 'r')
+        plt.figure()
+        ax = plt.subplot(projection='polar')
+        ax.set_theta_zero_location("N")
+        ax.set_theta_direction(-1)
+        ax.bar(angles, num_measurements, width=np.pi/(N_bins-1))
     plt.show()

27062a3605bfde55686d7567beba85d4b54fd685
Erik Wilthil <erik.wilthil@gmail.com>
2019-03-08 11:34:03 +0100

    Trackers now has clutter density values

--- detection_probability_analysis/run_tracking_mc2_simulation.py
+++ detection_probability_analysis/run_tracking_mc2_simulation.py
@@ -45,12 +45,68 @@ def plot_tracks_status(ax, track_file, track_status, true_states):
     ax.set_ylim(-setup.radar_range, setup.radar_range)
     ax.set_aspect('equal')
 
-def find_true_track(track_file, true_target):
+def find_true_track(track_file, true_target_measurements):
     true_target_index = -1
     for track_index, track in track_file.items():
-        if True:
+        N_total = 0
+        for estimate in track:
+            for measurement in estimate.measurements:
+                if measurement in true_target_measurements:
+                    N_total += 1
+        if N_total > 0.5:
             true_target_index = track_index
     return true_target_index
+
+def compare_ipdas(measurements_all, timestamps):
+    trivial_term = setup_trackers.autotrack.TrivialTrackTerminator()
+    measurement_model = setup_trackers.autocommon.CartesianMeasurementModel(setup_trackers.measurement_mapping, setup_trackers.simulated_measurement_covariance, setup_trackers.single_PD)
+    tracker = setup_trackers.autotrack.IPDAFTracker(setup_trackers.target_model, measurement_model, setup_trackers.track_gate, setup_trackers.survival_probability)
+    init = setup_trackers.autoinit.IPDAInitiator(tracker, setup_trackers.init_prob, setup_trackers.conf_threshold, term_threshold=0)
+    ipda_track_manager = setup_trackers.automanagers.Manager(tracker, init, trivial_term)
+
+    mc2_measurement_model = setup_trackers.autocommon.CartesianMeasurementModel(setup_trackers.measurement_mapping, setup_trackers.simulated_measurement_covariance, [setup_trackers.low_PD, setup_trackers.high_PD])
+    mc2_ipda_tracker = setup_trackers.autotrack.MC2IPDAFTracker(setup_trackers.target_model, mc2_measurement_model, setup_trackers.track_gate, setup_trackers.ipda_transition_matrix)
+    mc2_ipda_init = setup_trackers.autoinit.MC2IPDAInitiator(mc2_ipda_tracker, setup_trackers.markov_init_prob, setup_trackers.conf_threshold, term_threshold=0)
+    ipda_mc2_manager = setup_trackers.automanagers.Manager(mc2_ipda_tracker, mc2_ipda_init, trivial_term)
+
+    titles = ['MC1-IPDA', 'DET-IPDA']
+    current_managers = {'MC1-IPDA' : ipda_track_manager, 'DET-IPDA' : ipda_mc2_manager}
+    colors = {'MC1-IPDA' : 'b', 'DET-IPDA' : 'r'}
+    prelim_ext_prob = {title : dict() for title in titles}
+    conf_ext_prob = {title : dict() for title in titles}
+    preliminary_tracks_all = {title : dict() for title in titles}
+    for measurements, timestamp in zip(measurements_all, timestamps):
+        for title, manager in current_managers.items():
+            current_estimates, new_tracks = manager.step(measurements, timestamp)
+            current_preliminary_tracks = manager.initiation_method.preliminary_tracks
+            for track_id, track in current_preliminary_tracks.items():
+                preliminary_tracks_all[title][track_id] = track
+            for track_id, track in current_preliminary_tracks.items():
+                if track_id not in prelim_ext_prob[title].keys():
+                    prelim_ext_prob[title][track_id] = ([], [])
+                prelim_ext_prob[title][track_id][0].append(track[-1].timestamp)
+                prelim_ext_prob[title][track_id][1].append(track[-1].get_existence_probability())
+            for track_id, track in manager.track_file.items():
+                if track_id not in conf_ext_prob[title].keys():
+                    conf_ext_prob[title][track_id] = ([], [])
+                conf_ext_prob[title][track_id][0].append(track[-1].timestamp)
+                conf_ext_prob[title][track_id][1].append(track[-1].get_existence_probability())
+
+    pos_fig, pos_ax = plt.subplots()
+    autovis.plot_measurements(measurements_all, pos_ax)
+    for title, manager in current_managers.items():
+        autovis.plot_track_pos(manager.track_file, pos_ax, color=colors[title])
+        autovis.plot_track_pos(preliminary_tracks_all[title], pos_ax, color=colors[title])
+        pos_ax.set_aspect('equal')
+    ext_fig, ext_ax = plt.subplots()
+    for title, ext_prob_dict in prelim_ext_prob.items():
+        for track_id, data in ext_prob_dict.items():
+            ext_ax.plot(data[0], data[1], color=colors[title], lw=2, ls='--')
+    for title, ext_prob_dict in conf_ext_prob.items():
+        for track_id, data in ext_prob_dict.items():
+            ext_ax.plot(data[0], data[1], color=colors[title], lw=2, ls='-')
+        # Plot and stuff
+    plt.show()
 
 if __name__ == '__main__':
     _, _, _, _, _, time = setup.generate_scenario()
@@ -59,7 +115,7 @@ if __name__ == '__main__':
     hmm_track_manager = setup_trackers.setup_hmm_ipda_manager(measurement_type=setup_trackers.SIMULATED)
     vanilla_ipda_mc2_manager = setup_trackers.setup_mc2_ipda_manager(measurement_type=setup_trackers.SIMULATED)
     ipda_mc2_manager = setup_trackers.setup_mcn_ipda_manager(measurement_type=setup_trackers.SIMULATED)
-    titles = ['IPDA', 'HMM-IPDA', 'MC2-IPDA', 'MCN-IPDA']
+    titles = ['MC1-IPDA', 'HMM-IPDA', 'MC2-IPDA', 'DET-IPDA']
     current_managers_list = [ipda_track_manager, hmm_track_manager, vanilla_ipda_mc2_manager, ipda_mc2_manager]
     current_managers = {title : manager for title, manager in zip(titles, current_managers_list)}
 
@@ -68,19 +124,31 @@ if __name__ == '__main__':
     duration_false_tracks = {title : [] for title in titles}
     existence_prob = {title: np.zeros((len(time), N_MC)) for title in titles}
     detection_prob = {title: np.zeros((len(time), N_MC)) for title in titles}
+    detection_prob_mode = {title: np.zeros((len(time), N_MC)) for title in titles}
 
-    ext_prob_fig, ext_prob_ax = plt.subplots()
-    term_pos_fig, term_pos_ax = plt.subplots()
-    area_fig, area_ax  = plt.subplots()
     for n_mc in range(N_MC):
         true_target, true_detectability, true_existence, measurements_clutter, measurements_all, measurement_timestamps = setup.generate_scenario()
+        true_target_measurements = set.union(*[z_all-z_clutter for z_all, z_clutter in zip(measurements_all, measurements_clutter)])
+        false_confirmed_track_measurements = {title: [] for title in titles}
         # Run tracking with clutter-only
         [manager.reset() for manager in current_managers.values()]
         for n_time, timestamp in enumerate(measurement_timestamps):
             measurements = measurements_clutter[n_time]
             for title, manager in current_managers.items():
-                current_estimates, _ = manager.step(measurements, timestamp)
+                current_estimates, new_tracks = manager.step(measurements, timestamp)
+                for track in new_tracks:
+                    track_measurements = [estimate.measurements for estimate in track]
+                    false_confirmed_track_measurements[title].append(track_measurements)
                 num_false_tracks[title][n_time][n_mc] = len(current_estimates)
+        for title, manager in current_managers.items():
+            print "{} manager has {} false tracks".format(title, len(manager.track_file))
+        if len(current_managers['MC1-IPDA'].track_file) > 0 and len(current_managers['DET-IPDA'].track_file) == 0:
+            confirmed_ipda_measurements = []
+            timestamps_ipda = []
+            for track in current_managers['MC1-IPDA'].track_file.values():
+                timestamps_ipda = [estimate.timestamp for estimate in track]
+                measurements_ipda = [estimate.measurements for estimate in track]
+                #compare_ipdas(measurements_ipda, timestamps_ipda)
         for title, manager in current_managers.items():
             [duration_false_tracks[title].append(len(est_list)) for est_list in manager.track_file.values()]
         # Run tracking with target
@@ -88,16 +156,21 @@ if __name__ == '__main__':
         for n_time, timestamp in enumerate(measurement_timestamps):
             measurements = measurements_all[n_time]
             for title, manager in current_managers.items():
-                current_estimates, _ = manager.step(measurements, timestamp)
-                true_track_index = find_true_track(manager.track_file, true_target)
+                current_estimates, new_tracks = manager.step(measurements, timestamp)
+                active_tracks = {track_id : manager.track_file[track_id] for track_id in manager.active_tracks}
+                true_track_index = find_true_track(active_tracks, true_target_measurements)
                 if true_track_index > 0: # True track is found. Extract the latest and greatest existence probability, detection probability and error
+                    num_true_tracks[title][n_time][n_mc] = 1
                     t_ext, p_ext = analyse_tracks.get_existence_probability([manager.track_file[true_track_index][-1]])
                     existence_prob[title][n_time][n_mc] = p_ext[0]
-                    t_det, p_det = analyse_tracks.get_detection_probability([manager.track_file[true_track_index][-1]], manager.tracking_method.measurement_model.get_detection_probability(true_track_index))
+                    t_det, p_det = analyse_tracks.get_detection_probability_mean([manager.track_file[true_track_index][-1]], manager.tracking_method.measurement_model.get_detection_probability(true_track_index))
                     detection_prob[title][n_time][n_mc] = p_det[0]
+                    t_det, p_det = analyse_tracks.get_detection_probability_mode([manager.track_file[true_track_index][-1]], manager.tracking_method.measurement_model.get_detection_probability(true_track_index))
+                    detection_prob_mode[title][n_time][n_mc] = p_det[0]
                 else:
                     existence_prob[title][n_time][n_mc] = np.nan
                     detection_prob[title][n_time][n_mc] = np.nan
+                    detection_prob_mode[title][n_time][n_mc] = np.nan
         print "completed run {}/{}".format(n_mc+1, N_MC)
 
     markers = {title : marker for title, marker in zip(titles, ['s', 'o', 'd', 'v'])}
@@ -116,7 +189,9 @@ if __name__ == '__main__':
     detection_ax.plot(measurement_timestamps, true_detectability, 'k')
     for title, detection_values in detection_prob.items():
         det_val = np.nanmean(detection_values, axis=1)
-        detection_ax.plot(measurement_timestamps, det_val, label=title, marker=markers[title], markevery=20, lw=2)
+        det_val_mode = np.nanmean(detection_prob_mode[title], axis=1)
+        l = detection_ax.plot(measurement_timestamps, det_val, label=title, marker=markers[title], markevery=20, lw=2)
+        detection_ax.plot(measurement_timestamps, det_val_mode, label=title, marker=markers[title], markevery=20, lw=2, color=l[0].get_color(),ls='--')
     detection_ax.legend(loc='best')
     detection_ax.set_xlabel('time')
     detection_ax.set_ylim(0, 1)
@@ -144,14 +219,14 @@ if __name__ == '__main__':
     num_false_ax.set_xlabel('Time')
     num_false_ax.grid()
     
-    duration_fig, duration_ax = setup_figure()
-    durations = [duration_false_tracks[title] for title in titles]
-    duration_ax.hist(durations, label=titles)
-    duration_ax.set_xlabel('False track duration (scans)')
-    duration_ax.legend(loc='best')
-    duration_ax.grid()
+    #duration_fig, duration_ax = setup_figure()
+    #durations = [duration_false_tracks[title] for title in titles]
+    #duration_ax.hist(durations, label=titles)
+    #duration_ax.set_xlabel('False track duration (scans)')
+    #duration_ax.legend(loc='best')
+    #duration_ax.grid()
 
     num_true_fig.savefig('figs/average_number_true_targets.pdf')
     num_false_fig.savefig('figs/average_number_false_targets.pdf')
-    duration_fig.savefig('figs/false_track_duration.pdf')
+    #duration_fig.savefig('figs/false_track_duration.pdf')
     plt.show()
--- detection_probability_analysis/setup_trackers.py
+++ detection_probability_analysis/setup_trackers.py
@@ -6,6 +6,8 @@ import autoseapy.track_management as automanagers
 import autoseapy.track_initiation as autoinit
 import autoseapy.hidden_markov_model as hmm_models
 
+import generate_single_target_scenario as gen_scen
+
 target_process_noise_covariance = 0.05**2
 gate_probability = 0.99
 maximum_velocity = 15
@@ -47,13 +49,15 @@ target_model = setup_target_model(target_process_noise_covariance)
 def setup_track_gate(gate_prob, max_vel):
     return autocommon.TrackGate(gate_prob, max_vel)
 track_gate = setup_track_gate(gate_probability, maximum_velocity)
+
+clutter_model = autocommon.ConstantClutterModel(gen_scen.clutter_density)
     
 # Vanilla IPDAs
 def setup_ipda_manager(measurement_type=REAL):
     if measurement_type == REAL:
         measurement_model = autocommon.ConvertedMeasurementModel(measurement_mapping, measurement_covariance_range, measurement_covariance_bearing, single_PD, min_cartesian_cov)
     else:
-        measurement_model = autocommon.CartesianMeasurementModel(measurement_mapping, simulated_measurement_covariance, single_PD)
+        measurement_model = autocommon.CartesianMeasurementModel(measurement_mapping, simulated_measurement_covariance, single_PD, clutter_model=clutter_model)
     tracker = autotrack.IPDAFTracker(target_model, measurement_model, track_gate, survival_probability)
     init = autoinit.IPDAInitiator(tracker, init_prob, conf_threshold, term_threshold)
     term = autotrack.IPDATerminator(term_threshold)
@@ -67,7 +71,7 @@ def setup_hmm_ipda_manager(measurement_type=REAL):
     if measurement_type == REAL:
         measurement_model = autocommon.ConvertedMeasurementModelMarkovDetectionProbability(measurement_mapping, measurement_covariance_range, measurement_covariance_bearing, detection_model, min_cartesian_cov)
     else:
-        measurement_model = autocommon.CartesianMeasurementModelMarkovDetectionProbability(measurement_mapping, simulated_measurement_covariance, detection_model)
+        measurement_model = autocommon.CartesianMeasurementModelMarkovDetectionProbability(measurement_mapping, simulated_measurement_covariance, detection_model, clutter_model=clutter_model)
     tracker = autotrack.IPDAFTracker(target_model, measurement_model, track_gate, survival_probability)
     init = autoinit.IPDAInitiator(tracker, init_prob, conf_threshold, term_threshold)
     term = autotrack.IPDATerminator(term_threshold)
@@ -79,7 +83,7 @@ def setup_mc2_ipda_manager(measurement_type=REAL):
     if measurement_type == REAL:
         van_mc2_measurement_model = autocommon.ConvertedMeasurementModel(measurement_mapping, measurement_covariance_range, measurement_covariance_bearing, [0, single_PD], min_cartesian_cov)
     else:
-        van_mc2_measurement_model = autocommon.CartesianMeasurementModel(measurement_mapping, simulated_measurement_covariance, [0, single_PD])
+        van_mc2_measurement_model = autocommon.CartesianMeasurementModel(measurement_mapping, simulated_measurement_covariance, [0, single_PD], clutter_model=clutter_model)
     vanilla_mc2_ipda_tracker = autotrack.MC2IPDAFTracker(target_model, van_mc2_measurement_model, track_gate, ipda_transition_matrix)
     vanilla_mc2_ipda_init = autoinit.MC2IPDAInitiator(vanilla_mc2_ipda_tracker, markov_init_prob, conf_threshold, term_threshold)
     vanilla_mc2_ipda_term = autotrack.MC2IPDATerminator(term_threshold)
@@ -91,7 +95,7 @@ def setup_mcn_ipda_manager(measurement_type=REAL):
     if measurement_type == REAL:
         mc2_measurement_model = autocommon.ConvertedMeasurementModel(measurement_mapping, measurement_covariance_range, measurement_covariance_bearing, [low_PD, high_PD], min_cartesian_cov)
     else:
-        mc2_measurement_model = autocommon.CartesianMeasurementModel(measurement_mapping, simulated_measurement_covariance, [low_PD, high_PD])
+        mc2_measurement_model = autocommon.CartesianMeasurementModel(measurement_mapping, simulated_measurement_covariance, [low_PD, high_PD], clutter_model=clutter_model)
     mc2_ipda_tracker = autotrack.MC2IPDAFTracker(target_model, mc2_measurement_model, track_gate, ipda_transition_matrix)
     mc2_ipda_init = autoinit.MC2IPDAInitiator(mc2_ipda_tracker, markov_init_prob, conf_threshold, term_threshold)
     mc2_ipda_term = autotrack.MC2IPDATerminator(term_threshold)
